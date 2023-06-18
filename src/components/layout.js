f871ebf9cc4e5caa297c076c186b04be3a406dd8
gerald <geraldnyeo@gmail.com>
2023-06-18 12:25:38 +0800

    Production Bug Fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -16,7 +16,7 @@ export default function Layout(props) {
 
                 {/* Fonts */}
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&family=Roboto:wght@100&display=swap" rel="stylesheet" />
             </Head>
 
